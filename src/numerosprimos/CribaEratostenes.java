0ff7e6a47ac62c63a5726c95cb68077059240d06
laura <laura@192.168.1.131>
2023-03-04 02:59:20 +0100

    proyecto java numeros primos refactorizado

--- src/numerosprimos/CribaEratostenes.java
+++ src/numerosprimos/CribaEratostenes.java
@@ -1,68 +1,57 @@
 /**
  * Clase para generar todos los números primos de 1 hasta
  * un número máximo especificado por el usuario.
- */ 
-
-package numerosprimos;    
-    
-public class CribaEratostenes 
-{ 
- /** 
- * Generar números primos de 1 a max
- * @param max es el valor máximo
- * @return Vector de números primos
  */
-    
- public static int[] generarPrimos (int max) 
- { 
- int i,j; 
- if (max >= 2) { 
- 
- // Declaraciones
- int dim = max + 1; // Tamaño del array 
- boolean[] esPrimo = new boolean[dim]; 
- 
- // Inicializar el array
- for (i=0; i<dim; i++) 
- esPrimo[i] = true; 
- 
- // Eliminar el 0 y el 1, que no son primos
- esPrimo[0] = esPrimo[1] = false; 
- 
- // Criba
- for (i=2; i<Math.sqrt(dim)+1; i++) { 
- if (esPrimo[i]) {
- // Eliminar los múltiplos de i
- for (j=2*i; j<dim; j+=i) 
- esPrimo[j] = false; 
- } 
- }
- 
- // ¿Cuántos primos hay? 
- int cuenta = 0; 
- for (i=0; i<dim; i++) { 
- if (esPrimo[i]) 
- cuenta++; 
- } 
- // Rellenar el vector de números primos
- int[] primos = new int[cuenta]; 
- for (i=0, j=0; i<dim; i++) { 
- if (esPrimo[i])
- primos[j++] = i; 
- } 
- 
- return primos; 
- 
- } else { // max < 2 
- 
- return new int[0]; // Vector vacío
- } 
- }
+package numerosprimos;
+
+import java.util.Arrays;
+
+public class CribaEratostenes {
+
+    /**
+     * Generar números primos de 1 a max
+     *
+     * @param max es el valor máximo
+     * @return Vector de números primos
+     */
+
+    public static int[] generarPrimos(int max) {
+
+        if (max < 2) {
+            return new int[0];//no hay primos, devuelve array vacio
+        }
+        boolean[] esPrimo = new boolean[max + 1];
+        Arrays.fill(esPrimo, true);//rellenamos el array como primo(true) excepto el 0 y el 1
+        esPrimo[0] = false;
+        esPrimo[1] = false;
+
+//se implementa el algoritmo de la criba de eratostenes
+        for (int i = 2; i * i <= max; i++) {
+            if (esPrimo[i]) {
+                for (int j = i * i; j <= max; j += i) {
+                    esPrimo[j] = false;
+                }
+            }
+        }
+
+//se cuenta el numero de primos que hay
+        int cuenta = 0;
+        for (boolean primo : esPrimo) {
+            if (primo) {
+                cuenta++;
+            }
+        }
+
+//se crea el array de primos
+        int[] primos = new int[cuenta];
+        int idx = 0;
+        for (int i = 0; i <= max; i++) {
+            if (esPrimo[i]) {//si es primo se agrega al array
+                primos[idx++] = i;
+            }
+        }
+
+        return primos;
+
+    }
 }
-
-
-
-
-
-
-
